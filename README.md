d78be2e54a61240d8a021054976d496c1559a786
Andreea <andreeafarcas2803@gmail.com>
2024-03-26 16:07:40 +0200

    updated with the functionalities

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # The Hangman game
 
-- built with HTML, CSS and JavaScript
+The Hangman game provides a fun experience for players to test their word-guessing skills. With its intuitive interface and clear win/loss conditions, players can enjoy a classic game without unnecessary complexity.
+
+It is based on a tutorial from [20 Web Projects With Vanilla JavaScript Course](https://www.udemy.com/course/web-projects-with-vanilla-javascript/?couponCode=GENAISALE24)
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Built with](#built-with)
+
+## Overview
+
+Hangman is a user-friendly word-guessing game where the objective is to guess a hidden word by typing letters on your keyboard. You have 6 chances to guess incorrectly before you lose. If you guess the word correctly within those chances, you win the game. After winning or losing, you can restart the game with a simple button press.
+
+### Screenshot
+
+![](./image/already%20entered%20letter.png)
+![](./image/loose.png)
+![](./image/win.png)
+
+### Links
+
+- Live Site URL:
+
+### Built with
+
+- HTML
+- CSS
+- JavaScript
---
+++ script.js
@@ -0,0 +1,118 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const finalMessageRevealWord = document.getElementById(
+  'final-message-reveal-word'
+);
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        (letter) =>
+          `<span class="letter">${
+            correctLetters.includes(letter) ? letter : ''
+          } </span>`
+      )
+      .join('')}
+  `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won! 🤩';
+    popup.style.display = 'flex';
+  }
+}
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map((letter) => `<span> ${letter}</span>`)}
+    `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Sorry! Unfortunately you lost. 😭';
+    finalMessageRevealWord.innerText = `...the word was : ${selectedWord}`;
+    popup.style.display = 'flex';
+  }
+}
+
+// Show notifications
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// Event Listeners - keydown letter press
+window.addEventListener('keydown', (e) => {
+  if (e.code.toLowerCase().startsWith('key')) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
+});
+
+displayWord();
